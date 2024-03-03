15e27fdc445e25f513540c613f10c4e565281782
jinddobaegi <wlsehfdl95@naver.com>
2024-03-04 03:47:45 +0900

    jinyeong boj 1987 시간초과 240304

---
+++ week4/1987_jinyeong.py
@@ -0,0 +1,38 @@
+from sys import stdin
+input = stdin.readline
+
+R, C = map(int, input().split())
+
+arr = list(list(input().strip()) for _ in range(R))
+visited = list()
+visited.append(arr[0][0])
+
+dr = [1,-1,0,0]
+dc = [0,0,-1,1]
+
+
+def bfs(r, c):
+    global max_v
+    global cnt
+
+    cnt += 1
+    if max_v < cnt:
+        max_v = cnt
+
+    for k in range(4):
+        nr = r + dr[k]
+        nc = c + dc[k]
+        if 0 <= nr < R and 0 <= nc < C:
+            if arr[nr][nc] not in visited:
+                visited.append(arr[nr][nc])
+                bfs(nr, nc)
+                visited.pop()
+                cnt -= 1
+
+
+max_v = 0
+cnt = 0
+
+bfs(0,0)
+
+print(max_v)
