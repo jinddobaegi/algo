f3c0330fb13df3c2dde79fbe39016807280626bc
정진영 <wlsehfdl95@naver.com>
2024-03-19 10:07:01 +0900

    jin boj 1707 solved 240319

--- week6/1707/1707_jin.py
+++ week6/1707/1707_jin.py
@@ -13,7 +13,28 @@ input = stdin.readline
 # 그럼 현재 정점이 속한 집합도 알아야 하고
 # 인접 노드의 방문 여부와 집합 일치 여부를 확인해야 함
 
+
 K = int(input())
+
+
+def bfs(node, group_num):
+    # bfs를 돌면서
+    # 인접 노드들을 이미 방문한 경우
+    # 같은 집합에 속해있는지를 확인해야 함
+    q.append(node)
+    visited[node] = group_num
+    while q:
+        x = q.popleft()
+        for y in adj_list[x]:  # 인접 노드에 대해
+            if visited[y] == 0:  # 방문 안한 경우
+                q.append(y)
+                visited[y] = visited[x] * (-1)  # 방문 표시하며 집합까지 표시
+            else:  # 방문한 경우? -> 집합 확인
+                # 집합이 같다? NO
+                if visited[x] == visited[y]:
+                    return 'NO'
+    return 'YES'
+
 
 for tc in range(K):
     V, E = map(int, input().split())
@@ -25,43 +46,22 @@ for tc in range(K):
         adj_list[v].append(u)
         adj_list[u].append(v)
 
-    visited = [0]*(V+1)
+    '''
+    이걸로 집합이 같은지 다른지 표시하니까 시간초과 났었음
     list_a = []
     list_b = []
+    
+    visited에 1과 -1을 표시하는 방법으로 같은 집합인지 다른 집합인지 확인하더라
+    '''
 
     # bfs
     q = deque()
-    # 항상 1번부터 시작, 1번 노드는 a에 담을 것임
-    # 노드 번호와 함께 list_a/b 중 어디에 속하는지 정보 같이 넘김
-    # a: 0, b: 1
-    q.append((1, 0))
-    visited[1] = 1
-    list_a.append(1)
-
-    # bfs 돌면서 다음 노드의 방문 여부와
-    # 현재 노드와 집합 비교 할 것
-    # 조건이 안 맞으면 'NO' 출력
-    res = 'YES'
-
-    while q and res == 'YES':
-        v, v_num = q.popleft()
-
-        for w in adj_list[v]:
-
-            if not visited[w]:     # 다음 노드 미방문한 경우
-                visited[w] = 1
-                if v_num == 0:  # 현재 노드가 a에 속하는 경우
-                    q.append((w, 1))
-                    list_b.append(w)
-                else:              # 현재 노드가 b에 속하는 경우
-                    q.append((w, 0))
-                    list_a.append(w)
-
-            else:                  # 다음 노드를 이미 방문한 경우
-                # 인접 노드가 현재 노드와 같은 집합에 있는지 확인
-                # 같은 집합이면 -> 'NO'
-                if (v_num and w in list_b) or (not v_num and w in list_a):
-                    res = 'NO'
-                    break
+    visited = [0]*(V+1)
+    res = ''
+    for i in range(1, V+1):
+        if visited[i] == 0:
+            res = bfs(i, 1)
+            if res == 'NO':
+                break
 
     print(res)
