19240043ed7b966d744012d493d846b5f73dedbb
정진영 <wlsehfdl95@naver.com>
2024-03-30 16:30:40 +0900

    jin boj 11779 solved 240330

---
+++ week8/11779/11779_jin.py
@@ -0,0 +1,59 @@
+from sys import stdin
+from heapq import heappop, heappush
+
+input = stdin.readline
+
+N = int(input())
+M = int(input())
+
+graph = list([] for _ in range(N+1))
+for _ in range(M):
+    s, e, w = map(int, input().split())
+    graph[s].append((w, e))
+
+A, B = map(int, input().split())
+
+# dijkstra는 pq와 distance가 존재
+INF = int(1e9)
+pq = []
+distance = [INF] * (N+1)
+dij_route = [0] * (N+1)
+
+def dijkstra(start):
+    # 시작점 초기화
+    distance[start] = 0
+    heappush(pq, (0, start))
+
+    while pq:
+        dist, v = heappop(pq)
+
+        if distance[v] < dist:
+            continue
+
+        for cost, w in graph[v]:
+            new_dist = dist + cost
+
+            if distance[w] <= new_dist:
+                continue
+
+            distance[w] = new_dist
+            dij_route[w] = v
+            heappush(pq, (new_dist, w))
+
+
+dijkstra(A)
+
+print(distance[B])
+# print(dij_route)
+
+path = [B]
+now = B
+while now != A:
+    now = dij_route[now]
+    path.append(now)
+
+# print(path)
+# path = path[::-1]
+path.reverse()  # 리스트 길이가 1억개인 경우 '::-1'는 2초가 넘지만 reverse() 메서드는 0.2초가 안 걸림
+print(len(path))
+print(*path, sep=' ')
