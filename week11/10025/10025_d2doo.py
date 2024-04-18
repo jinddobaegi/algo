e120ac16082ff2d18aa42d43c44b4991d99184b0
d2doo <hzxvl@naver.com>
2024-04-18 09:32:10 +0900

    10025

--- week11/10025/10025_d2doo.py
+++ week11/10025/10025_d2doo.py
@@ -1,4 +1,5 @@
 # 게으른 백곰
+# 투 포인터?
 
 # 양동이 개수 N, 앨버트가 닿을 수 있는 거리 K
 N, K = map(int, input().split())
@@ -7,9 +8,24 @@ N, K = map(int, input().split())
 bucket = []
 for _ in range(N):
     ice, num = map(int, input().split())
-    bucket.append((ice, num))
+    bucket.append((num, ice)) # 순서 헷갈려서 (양동이 좌표, 얼음 양) 으로 입력
 
-bucket.sort(key=lambda x : x[1]) # 정렬했으니 - K 번째 양동이는 안 더해도됨
-print(bucket)
+bucket.sort() # 양동이 위치로 정렬
+# print(bucket) # [(1, 5), (2, 2), (7, 4), (15, 10)]
 
 # 거리 범위 내에 있으면 다 더해도 됨 ㅠㅠ
+sum_ice, max_ice = 0, 0
+start, end = 0, 0
+while start <= end and end < N:
+    if bucket[end][0] - bucket[start][0] <= 2 * K: # 좌우로 K니까 2K
+        sum_ice += bucket[end][1]
+        # print(f'end={end}')
+        # print(f'sum_ice={sum_ice}')
+        max_ice = max(sum_ice, max_ice)
+        end += 1
+    else:
+        sum_ice -= bucket[start][1]
+        start += 1
+        # 뺀게 최대합일리 없으니까 여기선 max계산 안함
+
+print(max_ice)
