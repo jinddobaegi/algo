91bd5a8df7a526e97777169bc1ca21f5d54db266
정진영 <wlsehfdl95@naver.com>
2024-05-28 17:45:19 +0900

    jin boj 14502 solving

--- week15/14502/14502_jin.py
+++ week15/14502/14502_jin.py
@@ -1,4 +1,7 @@
 from sys import stdin
+from itertools import combinations
+from collections import deque
+from copy import deepcopy
 
 input = stdin.readline
 
@@ -10,3 +13,65 @@ N, M = map(int, input().split())
 
 lab = [list(map(int, input().split())) for _ in range(N)]
 
+# 그냥 모든 경우의 벽을 세워보자
+com_list = []
+for i in range(N):
+    for j in range(M):
+        com_list.append((i, j))
+
+combs = list(combinations(com_list, 3))
+q = deque()
+
+
+def bfs(r, c):
+    q.append((r, c))
+    visited[r][c] = 1
+
+    while q:
+        r, c = q.popleft()
+
+        for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            nr = r + dr
+            nc = c + dc
+            if 0 <= nr < N and 0 <= nc < M:
+                if (lab[nr][nc] == 0 or lab[nr][nc] == 2) and visited[nr][nc] == 0:
+                    visited[nr][nc] = 1
+                    q.append((nr, nc))
+
+
+max_v = 0
+for comb in combs:
+    visited = list([0] * M for _ in range(N))
+    lab_copy = deepcopy(lab)
+    # comb 하나에 좌표 세 개 들어있음
+    # 그럼 그거에 벽 세웠을 때의 안전 구역을 각각 구해보자
+
+    flag = True
+    for y, x in comb:
+        if lab_copy[y][x] == 0:
+            lab_copy[y][x] = 1
+        else:
+            flag = False
+            break
+
+    if not flag:
+        continue
+
+    # 돌리고
+    for n in range(N):
+        for m in range(M):
+            if lab_copy[n][m] == 2:
+                bfs(n, m)
+    tmp = 0
+    for r in range(N):
+        for c in range(M):
+            if lab_copy[r][c] == 0:
+                tmp += 1
+
+    max_v = max(max_v, tmp)
+
+    # 다 돌면 복구
+    for y, x in comb:
+        lab_copy[y][x] = 0
+
+print(max_v)
