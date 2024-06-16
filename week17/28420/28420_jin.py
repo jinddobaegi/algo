91e1f4682de6cab2b52a2ce8ee74b8c56d237125
jinddobaegi <wlsehfdl95@naver.com>
2024-06-17 02:49:21 +0900

    jin boj 28420 solved 240617

--- week17/28420/28420_jin.py
+++ week17/28420/28420_jin.py
@@ -11,38 +11,61 @@ min_v = int(1e9)
 # b: 차 길이
 # c: 카라반 길이
 
-'''
-시간초과
 # 일자 하나, 기역자 둘
 # 1) 일자
 # (0, 0)에서 시작해서 첫 번째 값 구하고
 # 그냥 옮기면서 더하기 빼기만
-
 for i in range(N-a+1):
     for j in range(M-(b+c)+1):
         tmp = 0
+        flag = False
         # 좌상단 좌표 -> arr[i][j]
         for n in range(a):
             for m in range(b+c):
                 tmp += arr[i+n][j+m]
+                if tmp > min_v:
+                    flag = True
+                    break
+            if flag:
+                break
         min_v = min(min_v, tmp)
+
 
 # 2) 기역자
 # 카라반이랑 차 값을 따로 구하고
 # 옮기면서 더하기 빼기?
-
-
 def common_shape(x, y, z):  # z에
     global min_v
 
     for i in range(N-(x+y)+1):
-        for j in range(M-(x+z)+1):
+        for j in range(M-(z+x)+1):
             tmp = 0
+            flag = False
             # 좌상단 좌표 arr[i][j]
-            for n in range(x+y):
-                for m in range(x+z):
-                    if (n<x and m<z) or (n>=x and m>=z):
-                        tmp += arr[i+n][j+m]
+            for n in range(x):
+                for m in range(z):
+                    tmp += arr[i+n][j+m]
+                    if tmp > min_v:
+                        flag = True
+                        break
+                if flag:
+                    break
+
+            if flag:
+                continue
+
+            for n in range(y):
+                for m in range(x):
+                    tmp += arr[n+i+x][m+j+z]
+                    if tmp > min_v:
+                        flag = True
+                        break
+                if flag:
+                    break
+
+            if flag:
+                continue
+
             min_v = min(min_v, tmp)
 
 
@@ -51,4 +74,3 @@ if c != b:
     common_shape(a, c, b)
 
 print(min_v)
-'''
