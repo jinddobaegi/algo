367bac006e63ff285ddddd0994e01fbdc22ee25c
jinddobaegi <wlsehfdl95@naver.com>
2024-07-07 01:00:59 +0900

    jin boj 10971 failed 240707

---
+++ week19/10971/10971_jin.py
@@ -0,0 +1,39 @@
+from sys import stdin
+
+input = stdin.readline
+
+# N개의 도시
+# 출발지에서 모든 도시를 거쳐서 돌아와야 함
+# 출발지를 제외한 나머지 도시는 한 번만 거쳐갈 수 있음
+# 최소 비용
+
+
+def dfs(f, cnt, dist):
+    global min_v
+
+    if cnt == N:
+        min_v = min(dist, min_v)
+        return
+
+    for t, cost in adj_list[f]:
+        if not visited[t]:
+            visited[t] = 1
+            dfs(t, cnt+1, dist + cost)
+            visited[t] = 0
+
+
+N = int(input())
+adj_list = list([] for _ in range(N))
+for i in range(N):
+    tmp = tuple(map(int, input().split()))
+    for j, x in enumerate(tmp):
+        if x:
+            adj_list[i].append((j, x))
+
+# dfs로 가다가
+# 출발지 나오면 거리 갱신할 것임
+
+min_v = int(1e8)  # 최대가 1e7
+visited = [0] * N
+dfs(0, 0, 0)
+print(min_v)
