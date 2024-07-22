c83f53f6d1c21295cc771d497a9cecc83dca5fae
송찬의 <ctxpfj@naver.com>
2024-07-22 11:41:25 +0900

    chan

--- week21/2307/2307_chan.py
+++ week21/2307/2307_chan.py
@@ -18,38 +18,172 @@
 import sys
 sys.stdin = open("week21/2307/2307.txt")
 import heapq
+input = sys.stdin.readline
 
-def check1(start):
-    distance[start] = 0 
-    queue = ([0, start]) # 거리, 노드
+
+# 시간초과
+# def djikstra(start):
+#     queue = heapq
+#     queue = [(0, start)]
+#     distance[start] = 0
+
+#     while queue:
+#         dist, now = heapq.heappop(queue)
+
+#         if dist > distance[now]:
+#             continue
+        
+#         for next in range(1, N+1):
+#             if graph[now][next] != float("INF"):
+#                 tmp_dist = dist + graph[now][next]
+
+#                 if tmp_dist < distance[next]:
+#                     distance[next] = tmp_dist
+#                     heapq.heappush(queue, (tmp_dist, next))
+#                     move_image[next] = now
+                    
+
+# N, M = map(int, input().split()) # node의 갯수, 도로의 수
+# graph = [[float("INF")] * (N + 1) for _ in range(N+1)]
+# distance = [float("INF")] * (N+1)
+# move_image = [0] * (N + 1)
+
+# # for _ in graph:
+# #     print(_)
+
+# for _ in range(M): #2차원 인접행열에 시간 추가해주기
+#     a, b, t = map(int, input().split()) #도로a, b 와 통과 시간 t
+#     graph[a][b] = t
+#     graph[b][a] = t
+
+# djikstra(1)
+
+# # print(distance[N])
+# first = distance[N]
+# # print(move_image)
+
+# path = []
+# current = N
+# while current != 0:
+#     path.append(current)
+#     current = move_image[current]
+# path.reverse()
+# # print(path)
+
+# new = len(path)
+
+# result = float("INF")
+# delay = -1
+# for i in range(new - 1):
+#     distance = [float("INF")] * (N+1)
+#     a = path[i]
+#     b = path[i + 1]
+#     original_value = graph[a][b]
+#     graph[a][b] = float("INF")
+#     graph[b][a] = float("INF")
+
+#     djikstra(1)
+
+#     graph[a][b] = original_value
+#     graph[b][a] = original_value
+
+#     if distance[N] == float("INF"):
+#         delay = -1
+#         break
+#     else:
+#         delay = max(delay, distance[N] - first)
+
+# print(delay)
+
+def dijkstra(start):
+    distance[start] = 0
+    queue = [(0, start)]
 
     while queue:
-        c_dist, c_node = heapq.heappop(queue)
+        dist, now = heapq.heappop(queue)
 
-        if c_dist > distance[c_node]:
+        if dist > distance[now]:
             continue
 
-        for goal in range(nodes + 1):
-            if graph[c_node][goal] != float('inf'):
-                distance_to_goal = c_dist + graph[c_node][goal]
+        for next, weight in graph[now]:
+            tmp_dist = dist + weight
 
-                # 더 짧은 경로를 발견한 경우
-                if distance_to_goal < distance[goal]:
-                    print(distance[goal])
-                    distance[goal] = distance_to_goal
-                    heapq.heappush(queue, (distance_to_goal, goal))
+            if tmp_dist < distance[next]:
+                distance[next] = tmp_dist
+                heapq.heappush(queue, (tmp_dist, next))
+                move_image[next] = now
+
+def find_path():
+    path = []
+    current = N
+    while current != 0:
+        path.append(current)
+        current = move_image[current]
+    path.reverse()
+    return path
 
 
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
 
-
-nodes, loads = map(int, input().split())
-distance = [0] * (loads + 1)
-
-graph = [[0] * (nodes + 1) for _ in range(loads)]
-
-for _ in range(loads):
+for _ in range(M):
     a, b, t = map(int, input().split())
-    graph[a][b] = t
-    graph[b][a] = t
+    graph[a].append((b, t))
+    graph[b].append((a, t))
 
-check1(0)
+# 최초 다익스트라 알고리즘 수행
+distance = [float('inf')] * (N + 1)
+move_image = [0] * (N + 1)
+dijkstra(1)
+
+first = distance[N]
+
+path = find_path()
+
+max_delay = -1
+for i in range(len(path) - 1):
+    a = path[i]
+    b = path[i + 1]
+
+    for j in range(len(graph[a])):
+        if graph[a][j][0] == b:
+            original_value_a = graph[a][j]
+            graph[a][j] = (b, float('inf'))
+            break
+
+    for j in range(len(graph[b])):
+        if graph[b][j][0] == a:
+            original_value_b = graph[b][j]
+            graph[b][j] = (a, float('inf'))
+            break
+
+    new_distance = [float('inf')] * (N + 1)
+    distance = new_distance
+    dijkstra(1)
+
+    for j in range(len(graph[a])):
+        if graph[a][j][0] == b:
+            graph[a][j] = original_value_a
+            break
+
+    for j in range(len(graph[b])):
+        if graph[b][j][0] == a:
+            graph[b][j] = original_value_b
+            break
+
+    if distance[N] == float("inf"):
+        max_delay = -1
+        break
+    else:
+        max_delay = max(max_delay, distance[N] - first)
+
+print(max_delay)
+
+
+
+
+
+
+
+
+
