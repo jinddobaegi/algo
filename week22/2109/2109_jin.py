ecf3b98887278562cc32a1cdaf1ebc0732d3a5f0
jinyeong <wlsehfdl95@naver.com>
2024-07-29 11:18:38 +0900

    jin boj 2109 failed 240729

--- week22/2109/2109_jin.py
+++ week22/2109/2109_jin.py
@@ -1,30 +1,60 @@
 from sys import stdin
+from heapq import heappush, heappop
 
 input = stdin.readline
 
 N = int(input())  # 대학의 수
-lectures = []
+pq = []
 for _ in range(N):
     p, d = map(int, input().split())
-    # 금액을 음수로 넣어서 sort하면
-    # 절댓값이 제일 큰 수가 맨 앞으로 오게 하기 위해서
-    lectures.append((d, -p))
+    heappush((-p, d))
 
-lectures.sort()  # 기한 빠른 순, 액수 큰 순
+
+'''
+이게 안되네
+3
+10 1
+20 2
+30 2
+
+정답 50
+'''
 
 # d가 겹치는 강연들은
 # 그 기한 안에 몇 개를 할 수 있는지가 관건
+
+# 로직 2 -> 실패, 뭔가 이상함
+# 냅색처럼 풀어보자
+# i번째 강의에 대해
+# n일차에 최대로 얻을 수 있는 돈 계산
+
+# dp = [[0]*(N+1) for _ in range(N+1)]  # 행: 강의, 열: 날짜
+#
+# for i in range(1, N+1):
+#     for j in range(1, N+1):
+#         p, d = heappop(pq)
+#         p = -p
+#         # j일째에는 최대 j개의 강의 선택 가능함
+#         # 내가 j일째에 i번째 강의를 선택할 수 있는가?
+#         # 1) 선택 불가능
+#         if j < d:
+#             dp[i][j] = dp[i-1][j]
+#         # 2) 선택 가능
+#         else:
+
+
+# 로직 1 -> 실패
 # 마지막 날짜를 갱신하는 주기를 언제로 해줘야 할까?
 # 강연 기한 확인
 # -> 마지막 강연 날짜보다 작거나 같으면 못함(어차피 금액순으로 정렬돼있어서 날짜가 같으면 앞에꺼가 큰 거임)
 # 마지막 강연 날짜보다 크면 마지막 강연날짜+1 후 강연 함
 
-total = 0
-last_d = 0
-for d, p in lectures:
-    p = -p
-    if d > last_d:
-        total += p
-        last_d += 1
-
-print(total)
+# total = 0
+# last_d = 0
+# for d, p in lectures:
+#     p = -p
+#     if d > last_d:
+#         total += p
+#         last_d += 1
+#
+# print(total)
