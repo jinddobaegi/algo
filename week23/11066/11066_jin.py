6a1d6c2b5e9c2377fb1c04f752fb576d4cb6fae5
jinyeong <wlsehfdl95@naver.com>
2024-08-04 21:45:24 +0900

    jin boj 11066 solved 240804

--- week23/11066/11066_jin.py
+++ week23/11066/11066_jin.py
@@ -2,20 +2,23 @@ from sys import stdin
 
 input = stdin.readline
 
+# python3으로 시간초과, pypy로 되길래
+# python3으로 엄청 빠른 코드를 보니
+# 크누스 최적화 알고리즘이라는 게 있더라...
+# https://blog.naver.com/hands731/221809365746
+
 T = int(input())
+INF = int(1e9)
 for _ in range(T):
     K = int(input())
     arr = [0] + list(map(int, input().split()))
+    dp = [[0] * (K+1) for _ in range(K+1)]
     cumsum = [0] * (K+1)
     for i in range(1, K+1):
-        cumsum[i] = cumsum[i-1] + arr[i]
+        cumsum[i] = cumsum[i-1] + arr[i]  # 누적합 배열 만드는 코드
 
-    dp = [[int(1e9)] * (K+1) for _ in range(K+1)]  # min으로 갱신 위해 큰 수로 init
-    for i in range(1, K+1):
-        dp[i][i] = 0  # 한 페이지는 합치지 않기 때문에 비용이 0
-    
     '''
-    ij가
+    s,e가
     12 23 34
     13 24
     14
@@ -23,16 +26,11 @@ for _ in range(T):
     '''
     
     for gap in range(1, K):  # 간격: 1 ~ K-1
-        for s in range(1, K):  # 시작점: 1 ~ K-1
+        for s in range(1, K - gap + 1):
             e = s + gap
-            if e > K:
-                break
-            # dp[s][e]를 구하기 위해서는
-            # 여기서 중간 지점 p를 s~e까지 돌리면서
-            # dp[s][p-1]과 dp[p][e]의 합의 최솟값에
-            # s~e까지 누적합을 더해줘야 한다
-            cumsum_v = cumsum[e] - cumsum[s-1]
+            min_v = INF
             for p in range(s, e):
-                dp[s][e] = min(dp[s][e], dp[s][p] + dp[p+1][e] + cumsum_v)
+                min_v = min(min_v, dp[s][p] + dp[p+1][e])
+            dp[s][e] = min_v + (cumsum[e] - cumsum[s-1])
 
     print(dp[1][K])
