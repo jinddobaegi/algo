aba34300530e2f06f3620e226cf09e576dd2f053
송찬의 <ctxpfj@naver.com>
2024-09-02 14:20:54 +0900

    chan

--- week27/17281/17281_chan.py
+++ week27/17281/17281_chan.py
@@ -3,13 +3,55 @@ from itertools import permutations
 sys.stdin = open("week27/17281/17281.txt")
 input = sys.stdin.readline
 
+# def cal(lineup, innings):
+#     score = 0
+#     number = 0
+
+#     for inning in innings:
+#         out = 0
+#         bases = [0,0,0]
+
+#         while out < 3:
+#             point = inning[lineup[number]-1]
+
+#             if point == 0: # 아웃
+#                 out += 1
+#             elif point == 1: # 안타
+#                 score += bases[2] #3루에 있는사람이 점수로 들어옴 
+#                 bases = [1] + bases[:2] # 베이스를 새롭게 초기화
+#             elif point == 2: # 모든 주자가 2루씩 진루
+#                 score += bases[1] + bases[2] #2루와 3루
+#                 bases = [0,1] + bases[:1]
+#             elif point == 3:
+#                 score += sum(bases)
+#                 bases = [0,0,1]
+#             elif point == 4:
+#                 score += sum(bases) + 1
+#                 bases = [0,0,0]
+
+#             number = (number+1)%9 #이닝변경시 순번이동
+    
+#     return score
+ 
+# N = int(input())
+# innings = [list(map(int, input().split())) for _ in range(N)] 
+
+# origin = [2, 3, 4, 5, 6, 7, 8, 9]
+# max_score = 0
+# for perm in permutations(origin):
+#     lineup = list(perm[:3]) + [1] + list(perm[3:])
+
+#     max_score = max(max_score, cal(lineup, innings))
+
+# print(max_score)
+
 def cal(lineup, innings):
     score = 0
     number = 0
 
     for inning in innings:
         out = 0
-        bases = [0,0,0]
+        b1, b2, b3 = 0,0,0
 
         while out < 3:
             point = inning[lineup[number]-1]
@@ -17,19 +59,26 @@ def cal(lineup, innings):
             if point == 0: # 아웃
                 out += 1
             elif point == 1: # 안타
-                score += bases[2] #3루에 있는사람이 점수로 들어옴 
-                bases = [1] + bases[:2] # 베이스를 새롭게 초기화
+                score += b3 #3루에 있는사람이 점수로 들어옴 
+                # bases = [1] + bases[:2] # 베이스를 새롭게 초기화
+                b3 = b2
+                b2 = b1 
+                b1 = 1
             elif point == 2: # 모든 주자가 2루씩 진루
-                score += bases[1] + bases[2] #2루와 3루
-                bases = [0,1] + bases[:1]
-            elif point == 3:
-                score += sum(bases)
-                bases = [0,0,1]
-            elif point == 4:
-                score += sum(bases) + 1
-                bases = [0,0,0]
+                score += b2 + b3 #2루와 3루
+                # bases = [0,1] + bases[:1]
+                b3 = b1
+                b2 = 1 
+                b1 = 0
 
-            number = (number+1)%9 #이닝변경시 순번이동
+            elif point == 3:
+                score += b1 + b2 + b3
+                b1, b2, b3 = 0,0,1
+            elif point == 4:
+                score += b1 + b2 + b3 + 1
+                b1, b2, b3 = 0,0,0
+
+            number = (number+1)%9 #순번이동
     
     return score
  
@@ -44,4 +93,3 @@ for perm in permutations(origin):
     max_score = max(max_score, cal(lineup, innings))
 
 print(max_score)
-
